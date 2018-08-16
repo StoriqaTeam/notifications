7ab6ae08dc6aae6aa0e2fc8a5e8f92ca45eefedd
Alexander Andreev <andreevlex.as@gmail.com>
2018-08-16 12:15:32 +0300

    fix migration create_user_roles

--- migrations/2018-08-13-071527_create_user_roles/up.sql
+++ migrations/2018-08-13-071527_create_user_roles/up.sql
@@ -3,6 +3,8 @@ CREATE TABLE user_roles (
     id SERIAL PRIMARY KEY,
     user_id INTEGER NOT NULL,
     role VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT current_timestamp,
+    updated_at TIMESTAMP NOT NULL DEFAULT current_timestamp
 );
 
 CREATE UNIQUE INDEX user_roles_user_id_idx ON user_roles (user_id);

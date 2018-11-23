b27488e21b742844d5faa10eeb87ffe1999cfdbf
Alexander Andreev <andreevlex.as@gmail.com>
2018-11-23 16:04:21 +0300

    Added new templates in DB

---
+++ migrations/2018-11-23-123609_added-new-templates/down.sql
@@ -0,0 +1,4 @@
+DELETE FROM templates WHERE name = 'store_moderation_status_for_user';
+DELETE FROM templates WHERE name = 'base_product_moderation_status_for_user';
+DELETE FROM templates WHERE name = 'store_moderation_status_for_moderator';
+DELETE FROM templates WHERE name = 'base_product_moderation_status_for_moderator';

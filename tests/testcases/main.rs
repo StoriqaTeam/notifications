67cdd72b4b504a4a71db3a3a9727154da0f62115
Daniil Kolesnichenko <d.s.kolesnichenko@yandex.ru>
2019-01-23 16:58:27 +0300

    Move tests to one package to reduce compilation time (#76)

---
+++ tests/testcases/main.rs
@@ -0,0 +1,12 @@
+extern crate futures;
+extern crate hyper;
+extern crate notifications_lib as lib;
+extern crate serde_json;
+extern crate stq_http;
+extern crate stq_static_resources;
+extern crate stq_types;
+extern crate tokio_core;
+
+mod common;
+
+mod integration_test_templates;
